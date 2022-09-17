323e5066edf8adf46136f6f0b7fd986dd9390640
mohammed <saifurrahman0997@gmail.com>
2022-09-17 11:22:47 -0400

    updated packages

--- packages/hw2/src/first_node.py
+++ packages/hw2/src/first_node.py
@@ -1,103 +1,27 @@
+#!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
-import sys
-pi = 3.141592653589
-​
+
+
 def turtle_sq():
-	rospy.init_node('turtlesim_sq', anonymous=True)
-	pub = rospy.Publisher('/turtle1/cmd_vel',Twist, queue_size=10)
-	rate = rospy.Rate(10)
-	vel = Twist()
-	
-	#1
-	t = 0
-	while t < 20:
-	    vel.linear.x = 0.1
-	    vel.linear.y = 0
-	    vel.linear.z = 0
-	    vel.angular.x = 0
-	    vel.angular.y = 0
-	    vel.angular.z = 0
-	    pub.publish(vel)
-	    rate.sleep()
-	    t = t + 0.1
-	    
-	#2
-​
-	t = 0
-	while t<10:
-	    vel.linear.x = 0
-	    vel.linear.y = 0
-	    vel.linear.z = 0
-	    vel.angular.x = 0
-	    vel.angular.y = 0
-	    vel.angular.z = pi/2
-	    pub.publish(vel)
-	    rate.sleep()	
-	    t = t + 0.1	
-​
-	t = 0#3
-	while t < 20:
-	    vel.linear.x = 0.1
-	    vel.linear.y = 0
-	    vel.linear.z = 0
-	    vel.angular.x = 0
-	    vel.angular.y = 0
-	    vel.angular.z = 0
-	    pub.publish(vel)
-	    rate.sleep()	
-	    t = t + 0.1
-	
-	t = 0#4
-	while t<10:
-	    vel.linear.x = 0
-	    vel.linear.y = 0
-	    vel.linear.z = 0
-	    vel.angular.x = 0
-	    vel.angular.y = 0
-	    vel.angular.z = pi/2
-	    pub.publish(vel)
-	    rate.sleep()	
-	    t = t + 0.1	
-	   	
-	t=0#5
-	while t < 20:
-	    vel.linear.x = 0.1
-	    vel.linear.y = 0
-	    vel.linear.z = 0
-	    vel.angular.x = 0
-	    vel.angular.y = 0
-	    vel.angular.z = 0
-	    pub.publish(vel)
-	    rate.sleep()
-	    t = t + 0.1
-	   
-	t =0#6
-	while t<10:
-	    vel.linear.x = 0
-	    vel.linear.y = 0
-	    vel.linear.z = 0
-	    vel.angular.x = 0
-	    vel.angular.y = 0
-	    vel.angular.z = pi/2
-	    pub.publish(vel)
-	    rate.sleep()	
-	    t = t + 0.1	
-	    
-	t =0#7
-	while t < 20:
-	    vel.linear.x = 0.1
-	    vel.linear.y = 0
-	    vel.linear.z = 0
-	    vel.angular.x = 0
-	    vel.angular.y = 0
-	    vel.angular.z = 0
-	    pub.publish(vel)
-	    rate.sleep()
-	    t = t + 0.1
-​
+    rospy.init_node('first_node', anonymous=True)
+    pub = rospy.Publisher('/turtle1/cmd_vel',Twist, queue_size=10)
+    rate = rospy.Rate(10)
+    vel = Twist()
+    
+    while not rospy.is_shutdown():
+      vel.linear.x = 1
+      vel.linear.y = 0
+      vel.linear.z = 0
+      vel.angular.x = 0
+      vel.angular.y = 0
+      vel.angular.z = 0
+      pub.publish(vel)
+      rate.sleep()
+
+
 if __name__ == '__main__':
-	try:
-		turtle_sq()
-	except rospy.ROSInterruptException:
-		pass
+   try:
+     turtle_sq()
+   except rospy.ROSInterruptException:
+     pass
