e35ffef1b2fa5b86b3f537e1f58409dd720b1093
mohammed <saifurrahman0997@gmail.com>
2022-09-22 23:18:33 -0400

    updated

--- packages/homework2_ros/src/hw2_node.py
+++ packages/homework2_ros/src/hw2_node.py
@@ -5,13 +5,13 @@ from geometry_msgs.msg import Twist
 import sys
 
 
-def turtle_circle():
+def turtle_sq():
     rospy.init_node('turtlesim', anonymous=True)
     pub = rospy.Publisher('/turtle1/cmd_vel',	Twist, queue_size=10)
     rate = rospy.Rate(20)
     vel = Twist()
     
-    while not rospy.is_shutdown():
+    while 
         vel.linear.x = 1
         vel.linear.y = 0
         vel.linear.z = 0
@@ -20,7 +20,7 @@ def turtle_circle():
         vel.angular.z = 0
         #rospy.loginfo("Radius = %f",radius)
         pub.publish(vel)
-        rate.sleep(1)
+        
         
         
 
@@ -28,7 +28,6 @@ def turtle_circle():
 
 if __name__ == '__main__':
 	try:
-            turtle_circle()
+            turtle_sq()
 	except rospy.ROSInterruptException:
            pass
-
