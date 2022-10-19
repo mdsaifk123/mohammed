3486b595e916dd57d824d8561741160f916e55bd
mohammed <saifurrahman0997@gmail.com>
2022-10-18 23:51:12 -0400

    update

--- packages/homework2_ros/src/check_pram.py
+++ packages/homework2_ros/src/check_pram.py
@@ -1,9 +1,44 @@
 #!/usr/bin/env python3
 
 import rospy
+from turtlesim_helper.msg import UnitsLabelled
 
 
-if rospy.has_pram("mode"):
-    self.foo = rospy.get_pram("mode")
-else:
-    self.foo = "default"
+class check_pram:
+    def __init__(self):
+        if rospy.has_pram("converter"):
+            self.foo = rospy.get_pram("converter")
+        else:
+            self.foo = "default"
+
+        self.msg = UnitsLabelled()
+        self.msg.units = "smoots"
+        rospy.Subscriber("output1", UnitsLabelled, self.callback)
+        self.units = rospy.Publisher("output2", UnitsLabelled, queue_size=10)
+        self.units.publish(self.msg)
+
+
+    def callback(self,msg):
+        
+        #funtion  for conversion
+        meters = 0.3048*(msg.msg)
+        smoots = (msg.msg)/5.583
+        feet = msg.msg
+
+        if (param == "smoots"):
+            msg.units = smoots
+            self.units.publish(self.msg)
+
+        elif (param == "meters"):
+            msg.units = meters
+            self.units.publish(self.msg)
+
+        elif (param == "feet"):
+            msg.units = feet
+            self.units.publish(self.msg)
+
+
+if __name__=='__main__':
+    rospy.init_node('check_pram')
+    check_pram()
+    rospy.spin()
